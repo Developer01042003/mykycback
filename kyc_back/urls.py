a052f7d6a0d7eac54accec448d8ccfd9964d19e2
Vivek <99064174+Developer01042003@users.noreply.github.com>
2024-12-29 23:22:21 +0530

    Update urls.py

--- kyc_back/urls.py
+++ kyc_back/urls.py
@@ -22,3 +22,8 @@ urlpatterns = [
     path('auth/', include('users.urls')),
     path('kyc/', include('kyc.urls')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
